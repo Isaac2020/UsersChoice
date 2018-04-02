e8ebd762788a5dc28c53599f891fca99e37ef323
Isaac <Isaac2020>
2018-04-02 18:15:07 +0100

    Initial commit

---
+++ app/src/main/java/com/gdgminna/android/userschoice/EstherActivity.java
@@ -0,0 +1,43 @@
+package com.gdgminna.android.userschoice;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class EstherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_esther);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ArrayList<Item> items = new ArrayList<Item>();
+        items.add(new Item ("Rice", 100, R.drawable.michga_foods ));
+        items.add(new Item ("Rice", 100, R.drawable.michga_foods ));
+        items.add(new Item ("Rice", 100, R.drawable.michga_foods ));
+
+
+        ItemAdapter adapter = new ItemAdapter(this,items);
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(adapter);
+
+    }
+}
