8c42643980581b485d810a3544b1f4504b372e5f
Isaac <Isaac2020>
2018-04-02 18:27:00 +0100

    Initial commit

---
+++ app/src/main/java/com/gdgminna/android/userschoice/ItemAdapter.java
@@ -0,0 +1,42 @@
+package com.gdgminna.android.userschoice;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by MAGANI on 02/04/2018.
+ */
+
+public class ItemAdapter extends ArrayAdapter<Item>{
+
+    public ItemAdapter (Activity context, ArrayList<Item> Item){
+        super(context, 0,Item);
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+        Item currentItem = getItem(position);
+
+        TextView nameTextView = listItemView.findViewById(R.id.name_textview);
+        nameTextView.setText(currentItem.getName());
+
+        TextView pricetextView = listItemView.findViewById(R.id.price_textview);
+        pricetextView.setText(currentItem.getmPrice());
+
+        ImageView imageView = listItemView.findViewById(R.id.imgae);
+        imageView.setImageResource(currentItem.getmImageResourceId());
+
+        return listItemView;
+    }
+}
