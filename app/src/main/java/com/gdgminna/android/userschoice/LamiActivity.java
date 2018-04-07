7b0fe0b891fe3b6cf323b7f8ac941b3fa6eed3ca
Isaac <Isaac2020>
2018-04-07 23:56:16 +0100

    Updated

--- app/src/main/java/com/gdgminna/android/userschoice/LamiActivity.java
+++ app/src/main/java/com/gdgminna/android/userschoice/LamiActivity.java
@@ -1,8 +1,11 @@
 package com.gdgminna.android.userschoice;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -20,12 +23,21 @@ public class LamiActivity extends AppCompatActivity {
 
         ArrayList<Item> items = new ArrayList<>();
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
         items.add(new Item("Rice", "N100", R.drawable.rice));
-        items.add(new Item("Rice", "N100", R.drawable.egusi_soup));
-        items.add(new Item("Rice", "N200", R.drawable.chicken));
-        items.add(new Item("Rice", "N100", R.drawable.jollof));
-        items.add(new Item("Rice", "N100", R.drawable.meat));
-        items.add(new Item("Rice", "N150", R.drawable.amala));
+        items.add(new Item("Egusi soup", "N100", R.drawable.egusi_soup));
+        items.add(new Item("Chicken", "N200", R.drawable.chicken));
+        items.add(new Item("Jellof", "N100", R.drawable.jollof));
+        items.add(new Item("Beef", "N100", R.drawable.meat));
+        items.add(new Item("Amala & Ewedu soup", "N150", R.drawable.amala));
 
         ItemAdapter adapter = new ItemAdapter(this, items);
         ListView listView = findViewById(R.id.list);
